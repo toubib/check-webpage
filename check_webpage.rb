cfa8856d07a5b116d3883c1fa06cdde085177d45
Vincent Reydet <toubib.lag@gmail.com>
2009-05-04 23:22:01 +0000

    Added keyword check\nAdded https/ssl support\nAdded ARGV use\nAdded warn2 mode optionnal\nBugfixes

--- check_webpage.rb
+++ check_webpage.rb
@@ -1,20 +1,113 @@
 #!/usr/bin/env ruby
+#	
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#	Copyright Vincent Reydet
+
+#TODO
+# - check if threadsafe
+# - catch timeout error
 
 require 'net/http'
+require 'net/https'
 require 'open-uri'
 require 'rubygems'
 require 'hpricot'
+require 'optiflag'
 
-isDEBUG=0
-tWARN1=5
-tWARN2=10
-tCRITICAL=60
+#OPT PARSING
+module Example extend OptiFlagSet
+  usage_flag "h","help"
+  optional_switch_flag "v" do
+    description "verbose"
+  end
+  optional_switch_flag "vv" do
+    description "more verbose !"
+  end
+  optional_switch_flag "e" do
+    description "extended mode, see the documentation"
+  end
+  optional_flag "c" do
+    long_form "critical"
+    description "--critical, default 60,  Critical time (s)"
+  end
+  optional_flag "w" do
+    long_form "warn"
+    description "--warn, default 5,  warn time (s)"
+  end
+  optional_flag "w2" do
+    long_form "warn2"
+    description "--warn2, default 10,  warn2 time (s), use with '-e'"
+  end
+  optional_flag "k" do
+    long_form "key"
+    description "--key, check for keyword"
+  end
+  flag "u" do
+    long_form "url"
+    description "--url, absolute: [http://www.google.com]"
+  end
+
+  and_process!
+end 
+
+#VARS
+if ARGV.flags.e?
+  isEXTENDED=1
+else
+  isEXTENDED=0
+end
+
+if ARGV.flags.vv?
+  isDEBUG=2
+elsif ARGV.flags.v?
+  isDEBUG=1
+else
+  isDEBUG=0
+end
+
+if ARGV.flags.c?
+  tCRITICAL=ARGV.flags.c.to_f
+else
+  tCRITICAL=60
+end
+
+if ARGV.flags.w?
+  tWARN=ARGV.flags.w.to_f
+else
+  tWARN=5
+end
+
+if ARGV.flags.w2?
+  tWARN2=ARGV.flags.w2.to_f
+else
+  tWARN2=10
+end
+
+if ARGV.flags.k?
+  keyword=ARGV.flags.k
+else
+  keyword=nil
+end
+
+
 requestTimeout=180
 
-#  puts "Syntax error, exit ...\n\n * "+$0+" http://example.com"
+if isDEBUG >= 2;puts "\n * ARGS: c=#{tCRITICAL} w=#{tWARN} e=#{isEXTENDED} w2=#{tWARN2} u=#{ARGV.flags.u}";end
 
 begin
-  mainUrl = URI.parse(ARGV[0])
+  mainUrl = URI.parse(ARGV.flags.u)
 rescue
   puts "Critical: syntax error, can't parse url ..."
   retCodeLabel="Critical"
@@ -25,21 +118,75 @@ if mainUrl.path == "" || mainUrl.path == nil
   mainUrl.path = '/'
 end
 
+## Remove certificate warning
+#  http://www.5dollarwhitebox.org/drupal/node/64
+
+class Net::HTTP
+  alias_method :old_initialize, :initialize
+  def initialize(*args)
+    old_initialize(*args)
+    @ssl_context = OpenSSL::SSL::SSLContext.new
+    @ssl_context.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  end
+end
+
 ## PART 1 - get main page and parse it
 tStart = Time.now
 if isDEBUG >= 1;puts "\n * Get main page: #{mainUrl}";end
-
-h = Net::HTTP.new( mainUrl.host, 80)
+h = Net::HTTP.new( mainUrl.host, mainUrl.port)
+if mainUrl.scheme == "https"
+  h.use_ssl = true
+end
 h.read_timeout=requestTimeout
 resp, data = h.get(mainUrl.path, nil)
 
+#handle redirection
+i=0
+while resp.code == "302"
+  begin
+    mainUrl = URI.parse(resp['location'])
+  rescue
+    puts "Critical: can't parse redirected url ..."
+    exit 2
+  end
+  if isDEBUG >= 1;puts "   -> 302, main page is now: #{mainUrl}";end
+  h = Net::HTTP.new( mainUrl.host, mainUrl.port)
+  if mainUrl.scheme == "https"
+    h.use_ssl = true
+  end
+  h.read_timeout=requestTimeout
+  resp, data = h.get(mainUrl.path, nil)
+  i+=1
+  if i >= 5
+    if isDEBUG >= 1;puts "   -> too much redirect (5), exit";end
+    exit 2
+  end
+end
+
+#check return code
 if resp.code != "200"
   puts "Critical: main page rcode is #{resp.code} - #{resp.message}"
-  retCodeLabel="Critical"
   exit 2
 end
 
-if isDEBUG >= 1;puts "[#{resp.code}] #{resp.message} t(#{Time.now-tStart})";end
+#Check for keyword
+if keyword != nil
+  hasKey=0
+  data.each { |line|
+    if line =~ /#{keyword}/
+      hasKey=1
+    end
+  }
+  if hasKey==0
+    puts "Critical: string not found"
+    exit 2
+  end
+end
+
+#Get page size
+tsize=data.length
+
+if isDEBUG >= 1;puts "[#{resp.code}] #{resp.message} s(#{tsize}) t(#{Time.now-tStart})";end
 
 doc = Hpricot(data)
 parsingResult = doc.search("//img[@src]").map { |x| x['src'] }
@@ -82,14 +229,16 @@ if isDEBUG >= 2;puts "\n * remove duplicated links: #{linksToDlPrevCount} -> #{l
 
 ## PART 2 - DL content links
 tdl=0 #Stat
-tsize=0
 fileErrorCount=0
 if isDEBUG >= 1;puts "\n * downloading inner links (#{linksToDl.length}) ...";end
 threads = []
 linksToDl.each {  |link|
   threads << Thread.new(link) { |myLink|
       t0 = Time.now
-      h = Net::HTTP.new(myLink.host, 80)
+      h = Net::HTTP.new(myLink.host, myLink.port)
+      if mainUrl.scheme == "https"
+        h.use_ssl = true
+      end
       h.read_timeout=requestTimeout
       resp, data = h.get(myLink.path, nil)
       t1 = Time.now-t0
@@ -117,15 +266,20 @@ if isDEBUG >= 1
 end
 
 # set return
-if tTotal < tWARN1
+if tTotal < tWARN
   retCode=0
   retCodeLabel="OK"
-elsif tTotal >= tWARN1 && tTotal < tWARN2
+elsif !isEXTENDED && tTotal >= tWARN && tTotal < tCRITICAL
   retCode=1
   retCodeLabel="Warn"
-elsif tTotal >= tWARN2 && tTotal < tCRITICAL
+## - Extended mode begin
+elsif isEXTENDED && tTotal >= tWARN && tTotal < tWARN2
+  retCode=1
+  retCodeLabel="Warn"
+elsif isEXTENDED && tTotal >= tWARN2 && tTotal < tCRITICAL
   retCode=3
   retCodeLabel="Unknown"
+## - Extended mode end
 else
   retCode=2
   retCodeLabel="Critical"
