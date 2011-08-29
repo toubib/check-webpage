857c2a8573ba589a4ff9251219d502b28e50a091
Vincent Reydet <toubib.lag@gmail.com>
2011-08-29 20:29:12 +0000

    Added use of rvm. Now testing with ruby 1.8.7 and 1.9.2.

--- test_check_webpage.sh
+++ test_check_webpage.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+#
+# This script use rvm - http://beginrescueend.com
+#
 #TODO need a lot of more test !
+
+RVM_VERSIONS="1.8.7 1.9.2"
 
 green="\\033[1;32m"
 red="\033[1;31m"
 reset="\033[0m"
+
+function die {
+	echo $@
+	exit 1
+}
 
 function check_if_OK {
   OPTS=$1
@@ -35,15 +45,22 @@ function check_if_ERR {
   fi
 }
 
-echo
-check_if_OK "-u http://google.com"
-check_if_OK "-u http://google.com -vv"
-check_if_OK "-u http://google.com -k google"
-check_if_OK "-u http://google.com -k google -z"
-check_if_OK "-u http://google.com -k google -z -n"
+which rvm &>/dev/null || die "rvm not found"
 
-echo
-check_if_ERR "-u http://4dsHfNYD4KRyktGH.com" 
-check_if_ERR "-u http://google.com -k 4dsHfNYD4KRyktGH"
-
-echo
+for v in $RVM_VERSIONS
+do
+	rvm use $v
+	
+	echo
+	check_if_OK "-u http://google.com"
+	check_if_OK "-u http://google.com -vv"
+	check_if_OK "-u http://google.com -k google"
+	check_if_OK "-u http://google.com -k google -z"
+	check_if_OK "-u http://google.com -k google -z -n"
+	
+	echo
+	check_if_ERR "-u http://4dsHfNYD4KRyktGH.com" 
+	check_if_ERR "-u http://google.com -k 4dsHfNYD4KRyktGH"
+	
+	echo
+done
